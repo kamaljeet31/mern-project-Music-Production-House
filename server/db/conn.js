dafc19f151b2926ee3e88ab98179e44dceeecf2b
kamal <kamaljeetsingh1648@gmail.com>
2021-05-08 23:57:54 -0400

    initial commit

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,21 +1,3 @@
-// const mongoose = require('mongoose')
-
-// const DB =
-//   // 'mongodb+srv://kamaljeet:ojdsCHNLCjUo3zw9@cluster0.2wdme.mongodb.net/first-mern-project?retryWrites=true&w=majority'
-//   process.env.DATABASE
-// mongoose
-//   .connect(DB, {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('connection successful')
-//   })
-//   .catch((e) => {
-//     console.log('no connection')
-//   })
-
 const mongoose = require('mongoose')
 
 const DB = process.env.DATABASE
